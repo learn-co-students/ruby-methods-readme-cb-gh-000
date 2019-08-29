6314274b4fa1eac7d7ed8f3f3c276e3acfa7ae9a
ASHUTOSH SINGH <ashutoshs.ic18@nsut.ac.in>
2019-08-29 03:39:29 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,9 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def phrase
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+end
+phrase
+phrase
