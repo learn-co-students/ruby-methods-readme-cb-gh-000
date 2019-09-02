600f4c2db0687445ef4b8db2492983095e3d5dbd
Yussuf Bashir <hamzyuss1@gmail.com>
2019-09-02 18:44:19 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,27 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def greetings
+  puts "Hello World!"
+end
+
+option_field = "<fill your answer here:                >"
+puts "A group of flamingos is called #{option_field}? "
+
+answer = "Flamboyance"
+puts "The answer is + "+ answer +". "
+
+puts answer.upcase
+print answer.downcase
+
+3.times {puts "Hello World"}
+
+3.times {print "Hello world"}
+
+6+3
+
+total = 6+3
+total
+sub_total = total + 2334
+sub_total
