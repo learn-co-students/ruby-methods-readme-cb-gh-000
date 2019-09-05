da0c532a07cd0e2bd4518073724dfa6abc3bfddb
Sébastien Raoult <sebastien.raoult@epitech.eu>
2019-09-05 13:25:41 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,18 @@
 # Use this space to code along with the readme
+def say_hello_world_ten_times #Method Signature
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase             #Method Body
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end #Method Closing
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_world_ten_times
+
+say_hello_world_ten_times
